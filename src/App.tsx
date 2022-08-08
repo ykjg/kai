310718239b8c6af3b73bb78cb248ab7ad52bed80
jigsaw <m@jgs.me>
2022-08-08 22:00:06 +0900

    impl

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,50 @@
+import { useCallback, useMemo, useState } from "react";
+
 export const App = () => {
+  const [input, setInput] = useState("");
+  const onChange = useCallback<React.ChangeEventHandler<HTMLTextAreaElement>>(
+    (event) => {
+      setInput(event.target.value);
+    },
+    []
+  );
+  const output = useMemo(() => {
+    return input.split("\n").reduce((prev, curr) => {
+      if (prev.length === 0) {
+        return curr;
+      }
+      if (prev.match(/\-\s*$/)) {
+        return `${prev.replace(/\-\s*$/, "")}${curr}`;
+      }
+      return `${prev} ${curr.replace(/\s*$/, "")}`;
+    }, "");
+  }, [input]);
+  const onClick = useCallback<React.MouseEventHandler<HTMLTextAreaElement>>(
+    (event) => {
+      event.currentTarget.select();
+    },
+    []
+  );
   return (
-    <div className="font-bold text-4xl m-[2em] text-red-600 underline">
-      Hello World
+    <div className="w-full">
+      <div className="w-full p-4">
+        <textarea
+          onChange={onChange}
+          value={input}
+          className="w-full border border-solid border-gray-500 rounded p-2"
+          placeholder="input here"
+          rows={10}
+        ></textarea>
+      </div>
+      <div className="w-full p-4">
+        <textarea
+          className="w-full border border-solid border-gray-500 rounded p-2"
+          placeholder="output here"
+          rows={10}
+          value={output}
+          onClick={onClick}
+        ></textarea>
+      </div>
     </div>
   );
 };
