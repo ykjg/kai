ea14f0f19873c880c977e6a6273cc56f03685bdb
jigsaw <m@jgs.me>
2022-08-08 22:37:43 +0900

    Buttons

--- src/App.tsx
+++ src/App.tsx
@@ -32,6 +32,16 @@ export const App = () => {
     ref.current?.select();
     document.execCommand("copy");
   }, []);
+  const deeplLink = useMemo(() => {
+    return `https://www.deepl.com/translator#en/ja/${encodeURIComponent(
+      output
+    )}`;
+  }, [output]);
+  const googleLink = useMemo(() => {
+    return `https://translate.google.co.jp/?sl=en&tl=ja&text=${encodeURIComponent(
+      output
+    )}`;
+  }, [output]);
   return (
     <div className="w-full">
       <div className="w-full p-4">
@@ -44,6 +54,22 @@ export const App = () => {
         ></textarea>
       </div>
       <div className="flex p-4">
+        <a
+          href={deeplLink}
+          target="_blank"
+          rel="noreferer nofollow"
+          className="block w-4/12 border border-solid border-blue-900 rounded py-4 text-center mx-4"
+        >
+          deepl
+        </a>
+        <a
+          href={googleLink}
+          target="_blank"
+          rel="noreferer nofollow"
+          className="block w-4/12 border border-solid border-yellow-300 rounded py-4 text-center mx-4"
+        >
+          google
+        </a>
         <button
           className="block w-4/12 border border-solid border-red-400 rounded py-4"
           onClick={copy}
